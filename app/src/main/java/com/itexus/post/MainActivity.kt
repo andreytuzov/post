979cbc980aac5a7b0f0460d7cd2cfeb7eced1b08
andrey.tuzov <C#nVks23zi>
2021-08-30 14:09:22 +0300

    setup project

---
+++ app/src/main/java/com/itexus/post/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.itexus.post
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.itexus.post.ui.theme.PostTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PostTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    PostTheme {
+        Greeting("Android")
+    }
+}
